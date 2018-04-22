0ceb0840cb089f8a680f25af9f14f3ce9ba8ba4a
Wilhelm Törnroos <vilkku@gmail.com>
2018-04-22 09:51:01 +0300

    Reset dmage/heal selection on form close, improve initiative suggestion styles

--- client/Player/CombatantSuggestor.ts
+++ client/Player/CombatantSuggestor.ts
@@ -25,14 +25,16 @@ export class CombatantSuggestor {
 
     public Resolve = (form: HTMLFormElement) => {
         const element = $(form).find("[name=suggestedDamage]").first();
-        const value = parseInt(element.val().toString(), 10);
+        let value = parseInt(element.val().toString(), 10);
         if (!isNaN(value) && value !== 0) {
             if ($(form).find("[name=damageType]:checked").val() === 'healing') {
                 value = value * -1;
             }
 
             this.Socket.emit("suggest damage", this.EncounterId, [this.Combatant().Id], value, "Player");
+
         }
+        $(form).find("[name=damageType]").first().prop('checked', true);
         element.val("");
         this.Close();
     }
